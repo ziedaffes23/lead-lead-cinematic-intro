d58557aa2b970052a20297d5589a06c56761490e
ziedaffes23 <3.21946812e+08+ziedaffes23@users.noreply.github.com>
2026-08-30 17:58:35 +0000

    Use corrected Apps Script deployment

--- shared/sheetsEndpoint.ts
+++ shared/sheetsEndpoint.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_SHEETS_WEB_APP_URL =
-  "https://script.google.com/macros/s/AKfycbztUHdla2ycMjqGiXMoTpjTRAQ2QwZuCvLzSsHSw1EaKUjeh0Xte7_1gz_rfoWf2qY8BA/exec";
+  "https://script.google.com/macros/s/AKfycbw4AbEAqAdrUHhXaOOq2OoNJFKKhjpKQA8_SkWmhHNlYLh0cayQnIEUpTSXCO9S3H5kfw/exec";
 
 const LEGACY_SHEETS_WEB_APP_URLS = new Set([
   "https://script.google.com/macros/s/AKfycbwQ40qc9TDpAlz0g6GQ1-CbXDDkiMA3crhafU7pdIZxK-kOy7_lfZwdaphS8uax1l5IlQ/exec",
